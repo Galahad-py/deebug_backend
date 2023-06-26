2e3cdef461a4f9e78f5fb899d6e28874b28b1dc5
Galahad-py <kinglemuelalpha@gmail.com>
2023-06-26 13:12:54 +0100

    I added let to second question

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
 let age = 25;
 
-age >= 18 ? comsole.log("Eligible to vote") : console.log("not eligible to vote");
+age >= 18 ? console.log("Eligible to vote") : console.log("not eligible to vote");
+
+let no = 20
+
+no % 2 == 0 ? console.log("Number is even") : console.log("Number is odd");
