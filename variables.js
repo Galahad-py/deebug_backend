3ee8ac25a2122ddf7332f37e7d9f55bf838af324
Galahad-py <kinglemuelalpha@gmail.com>
2023-06-26 15:25:15 +0100

    if's statements added to class

--- variables.js
+++ variables.js
@@ -1,7 +1,37 @@
+/*question 1*/
 let age = 25;
 
-age >= 18 ? console.log("Eligible to vote") : console.log("not eligible to vote");
+/*age >= 18 ? console.log("Eligible to vote") : console.log("not eligible to vote");*/
 
+/*question 2*/
 let no = 20
 
-no % 2 == 0 ? console.log("Number is even") : console.log("Number is odd");
+/*no % 2 == 0 ? console.log("Number is even") : console.log("Number is odd");*/
+
+
+
+/*class for data types*/
+
+/*let a  = 10;
+let b = "10";
+console.log(typeof b);*/
+
+
+
+let score = 20;
+
+if (score >= 70 && score <= 100) {
+    console.log("A");
+}else if (score >= 60 && score <= 69) {
+    console.log("B");
+}else if (score >= 50 && score <= 59) {
+    console.log("C");
+}else if (score >= 40 && score <= 49) {
+    console.log("D");
+}else if (score >= 35 && score <= 39) {
+    console.log("E");
+}else if (score >= 0 && score <= 34) {
+    console.log("F");}
+else {
+    console.log("Marks cannot be allocated");
+}
